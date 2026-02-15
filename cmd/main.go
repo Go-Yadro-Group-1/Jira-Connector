3cc91d9a69e85ee914cd87b2644a7e11224b00ec
German <123095564+6ermvH@users.noreply.github.com>
2026-02-15 16:44:50 +0300

    Revert "add base main workflow"
    This reverts commit 3cef929d761b33d918f9b26d62f068a908c56e2f.

--- cmd/main.go
+++ cmd/main.go
@@ -1,38 +1,4 @@
 package main
 
-import (
-	"context"
-
-	"github.com/Go-Yadro-Group-1/Jira-Connector/internal/config"
-	"github.com/Go-Yadro-Group-1/Jira-Connector/internal/repository"
-	"github.com/Go-Yadro-Group-1/Jira-Connector/internal/server"
-	"github.com/Go-Yadro-Group-1/Jira-Connector/internal/service"
-	"github.com/jackc/pgx/v4"
-)
-
 func main() {
-	cfg, err := config.Load()
-	if err != nil {
-		return
-	}
-
-	repo, err := repository.NewRepository(cfg)
-	if err != nil {
-		return
-	}
-	defer repo.Close()
-
-	db, err := pgx.Connect(context.Background(), cfg.Database.DSN)
-	if err != nil {
-		return
-	}
-	defer db.Close(context.Background())
-
-	connectorService := service.NewConnectorService(cfg, repo)
-	server := server.NewServer(connectorService)
-
-	if err := server.Start(); err != nil {
-		return
-	}
-	defer server.Stop()
 }
